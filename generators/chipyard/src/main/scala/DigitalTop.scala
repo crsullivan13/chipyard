7db83139bfa08114a698ae82132f4c29fc7959e9
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-07-09 14:31:26 -0500

    Update digital top for name change

--- generators/chipyard/src/main/scala/DigitalTop.scala
+++ generators/chipyard/src/main/scala/DigitalTop.scala
@@ -34,7 +34,7 @@ class DigitalTop(implicit p: Parameters) extends ChipyardSystem
   with chipyard.clocking.HasChipyardPRCI // Use Chipyard reset/clock distribution
   with fftgenerator.CanHavePeripheryFFT // Enables optionally having an MMIO-based FFT block
   with constellation.soc.CanHaveGlobalNoC // Support instantiating a global NoC interconnect
-  with CanHavePeripheryBRU
+  with CanHavePeripheryLLCBRU
 {
   override lazy val module = new DigitalTopModule(this)
 }
